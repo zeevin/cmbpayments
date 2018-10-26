f249d5896d809ee771c4ae7b865aea7b9172c08c
ConnorCho <irestful@gmail.com>
2018-10-26 15:03:20 +0800

    添加config示例

---
+++ tests/config.sample.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @link   https://www.init.lu
+ * @author Cao Kang(caokang@outlook.com)
+ * Date: 2018/10/23
+ * Time: 10:45 AM
+ * Source: config.php
+ * Project: cmbpayments
+ */
+
+return [
+    'http'  =>
+        [
+            'allow_redirects' => true,
+        ],
+    'cmb'   =>
+        [
+            'privateKey'  => 'xxx',
+            'branchNo' => '0001',
+            'merchantNo'   => '001116',
+            'endpoint'=>[
+                'agreement'=>[
+                    'register'=>'http://121.15.180.66:801/mobilehtml/DebitCard/M_NetPay/OneNetRegister/NP_BindCard.aspx',
+                    'query'=>'http://121.15.180.72/CmbBank_B2B/UI/NetPay/DoBusiness.ashx',
+                    'cancel'=>'http://121.15.180.72/CmbBank_B2B/UI/NetPay/DoBusiness.ashx'
+                ],
+                'pubkey'=>'http://121.15.180.72/CmbBank_B2B/UI/NetPay/DoBusiness.ashx',
+                'order'=> [
+                    'pay'=>'http://121.15.180.66:801/NetPayment/BaseHttp.dll?MB_EUserPay',
+                    'query'=>'http://121.15.180.66:801/NetPayment_dl/BaseHttp.dll?QuerySingleOrder',
+                    'refund'=>'http://121.15.180.66:801/NetPayment_dl/BaseHttp.dll?DoRefund'
+                ],
+            ]
+        ],
+
+    'cache' =>
+        [
+            //http://doctrine-orm.readthedocs.io/projects/doctrine-orm/en/latest/reference/caching.html
+            'default'  => 'file',
+            'prefix' => 'cmbCache_',
+            'stores' => [
+                'file' => [
+                    'driver' => 'file',
+                    'patch' => __DIR__.DIRECTORY_SEPARATOR.'cache'
+                ],
+                'memcached' => [
+                    'server' => '127.0.0.1',
+                    'port' => 11211
+                ],
+                'redis' => [
+                    'driver' => 'predis',
+                    'host' => '127.0.0.1',
+                    'password' => null,
+                    'port' => 6379,
+                    'database'=> 0
+                ]
+            ]
+        ]
+];
